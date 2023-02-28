295210ea65fa3f920d69e5832b2a88cdfb1c1ec0
SiriusNEO <1713833595@qq.com>
2023-02-28 11:40:39 +0800

    [Op][Training] Support Batch Norm Train Mode

--- include/tvm/relax/transform.h
+++ include/tvm/relax/transform.h
@@ -266,6 +266,19 @@ TVM_DLL Pass RemoveUnusedFunctions(Array<runtime::String> entry_functions);
  */
 TVM_DLL Pass RunCodegen(Optional<Map<String, Map<String, ObjectRef>>> target_options,
                         Array<runtime::String> entry_functions);
+
+/*!
+ * \brief Rewriting some operators to its training mode.
+ *
+ * Some operators behave differently in eval mode and train mode (e.g. BatchNorm). Usually, there
+ * will be a training: bool flag to indicate whether the operator is in training mode. This pass can
+ * turn a relax function to "train mode" by setting the training flag as true for all this kind of
+ * operators.
+ *
+ * \return The Pass.
+ */
+TVM_DLL Pass ToTrainMode();
+
 /*!
  * \brief Reverse-mode automatic differentiation.
  *
